da2428c60c829d818a73213014f1c18abe1afe58
Ishan Pranav <64936129+ishanpranav@users.noreply.github.com>
2022-08-11 14:58:24 -0700

    Create ComplexConverter.cs

---
+++ ComplexConverter.cs
@@ -0,0 +1,79 @@
+// ComplexConverter.cs
+// Copyright (c) 2022 Ishan Pranav. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Numerics;
+
+namespace System.Text.Json.Serialization.Converters
+{
+    /// <summary>
+    /// Converts a <see cref="Complex"/> value to or from JSON.
+    /// </summary>
+    public class ComplexConverter : JsonConverter<Complex>
+    {
+        /// <inheritdoc/>
+        public override Complex Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            double real = 0;
+            double imaginary = 0;
+
+            while (reader.TokenType != JsonTokenType.EndObject)
+            {
+                reader.Read();
+
+                if (reader.TokenType == JsonTokenType.PropertyName)
+                {
+                    string? text = reader.GetString();
+
+                    if (text != null)
+                    {
+                        reader.Read();
+
+                        StringComparison comparisonType;
+
+                        if (options.PropertyNameCaseInsensitive)
+                        {
+                            comparisonType = StringComparison.OrdinalIgnoreCase;
+                        }
+                        else
+                        {
+                            comparisonType = StringComparison.Ordinal;
+                        }
+
+                        if (text.Equals(GetPropertyName(nameof(Complex.Real), options.PropertyNamingPolicy), comparisonType))
+                        {
+                            real = reader.GetDouble();
+                        }
+                        else if (text.Equals(GetPropertyName(nameof(Complex.Imaginary), options.PropertyNamingPolicy), comparisonType))
+                        {
+                            imaginary = reader.GetDouble();
+                        }
+                    }
+                }
+            }
+
+            return new Complex(real, imaginary);
+        }
+
+        /// <inheritdoc/>
+        public override void Write(Utf8JsonWriter writer, Complex value, JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+            writer.WriteNumber(GetPropertyName(nameof(Complex.Real), options.PropertyNamingPolicy), value.Real);
+            writer.WriteNumber(GetPropertyName(nameof(Complex.Imaginary), options.PropertyNamingPolicy), value.Imaginary);
+            writer.WriteEndObject();
+        }
+
+        private static string GetPropertyName(string propertyName, JsonNamingPolicy? namingPolicy)
+        {
+            if (namingPolicy == null)
+            {
+                return propertyName;
+            }
+            else
+            {
+                return namingPolicy.ConvertName(propertyName);
+            }
+        }
+    }
+}
