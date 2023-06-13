6aaadf0f6e0df5363438a0533e6c5969a3ac4b6b
Ishan Pranav <64936129+ishanpranav@users.noreply.github.com>
2023-06-13 13:40:13 -0700

    Update JsonCultureInfoConverter.cs

--- JsonCultureInfoConverter.cs
+++ JsonCultureInfoConverter.cs
@@ -1,5 +1,5 @@
 // JsonCultureInfoConverter.cs
-// Copyright (c) 2021-2022 Ishan Pranav. All rights reserved.
+// Copyright (c) 2021-2023 Ishan Pranav. All rights reserved.
 // Licensed under the MIT License.
 
 using System.Globalization;
